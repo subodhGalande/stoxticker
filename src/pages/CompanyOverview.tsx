25c4c82d0b0cef32351e510091fa758df6b627d8
Saloni Chouhan <salonichouhan226@gmail.com>
2021-05-08 22:18:21 +0530

    company overview started

--- src/pages/CompanyOverview.tsx
+++ src/pages/CompanyOverview.tsx
@@ -5,7 +5,12 @@ import {
   IonToolbar,
   IonTitle,
   IonText,
+  IonChip,
+  IonLabel,
+  IonIcon,
 } from "@ionic/react";
+
+import { cogOutline, earthOutline, cashOutline } from "ionicons/icons";
 
 import "./CompanyOverview.css";
 
@@ -13,15 +18,37 @@ const CompanyOverview: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Company Overview</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle>
+            <strong>Company Overview</strong>
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen={true}>
-        tavb11
-        <IonText>
-          <h1>Relaince Industries </h1>
-        </IonText>
+        <div className="CompanyOverview">
+          <IonText color="dark">
+            <h3>
+              <b>Reliance Industries</b>{" "}
+            </h3>
+          </IonText>
+          <IonText className="ion-text-uppercase" color="medium">
+            <h3 className="tickersymbol">rel</h3>
+          </IonText>
+          <div className="chips">
+            <IonChip color="primary">
+              <IonIcon icon={cogOutline} color="primary" />
+              <IonLabel>Sector</IonLabel>
+            </IonChip>
+            <IonChip color="secondary">
+              <IonIcon icon={cashOutline} color="secondary" />
+              <IonLabel>Capitalization</IonLabel>
+            </IonChip>
+            <IonChip color="tertiary">
+              <IonIcon icon={earthOutline} color="tertiary" />
+              <IonLabel>Exchange</IonLabel>
+            </IonChip>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
