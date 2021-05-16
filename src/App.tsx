8abe67aead72ddeef3d4f7db6bdf7fbbf3e14f87
subodh Galande <subodh.dev@outlook.com>
2021-05-16 16:45:33 +0530

    firebase login remaining

--- src/App.tsx
+++ src/App.tsx
@@ -35,35 +35,14 @@ import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
-
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
 
 /* Theme variables */
 import "./theme/variables.css";
 import React from "react";
 import axios from "axios";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBJGGZa3q6GY8vMkaBex6E6aTdDUR652Rs",
-  authDomain: "stoxticker.firebaseapp.com",
-  projectId: "stoxticker",
-  storageBucket: "stoxticker.appspot.com",
-  messagingSenderId: "261467124957",
-  appId: "1:261467124957:web:288582ae8f82144e3b9734",
-  measurementId: "G-3VGD86C8L8",
-};
-
 const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <IonApp>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -17,10 +17,55 @@ import {
   IonThumbnail,
   useIonViewWillEnter,
 } from "@ionic/react";
-
 import { logoGoogle } from "ionicons/icons";
-
 import "./Signup.css";
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+firebase.initializeApp(firebase);
+firebase.analytics();
+
+onsubmit = () => {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithRedirect(provider);
+
+  firebase
+    .auth()
+    .getRedirectResult()
+    .then((result) => {
+      if (result.credential) {
+        const credential = result.credential as firebase.auth.OAuthCredential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+};
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const Signup: React.FC = () => {
   return (
